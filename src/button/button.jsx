7838b07a012866542aac3d236face2bab53b9c42
Yeqin Gong <gon@gyeq.in>
2024-08-08 00:23:16 +0800

    update button title and style

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -1,15 +1,18 @@
 import classNames from 'classnames';
 import styles from './button.module.css';
 
-export function Button({ className, vertical, disabled, onClick, children }) {
+export function Button({ className, vertical, disabled, title, onClick, onMouseDown, onMouseUp, children }) {
   const handleClick = disabled ? () => {} : onClick;
   return (
     <button
       className={classNames(styles.outlined, className, {
         [styles.disabled]: disabled,
       })}
-      onClick={handleClick}
+      title={title}
       disabled={disabled}
+      onClick={handleClick}
+      onMouseUp={onMouseUp}
+      onMouseDown={onMouseDown}
     >
       <div
         className={classNames(styles.content, {
