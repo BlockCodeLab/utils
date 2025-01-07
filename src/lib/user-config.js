8c3593eb7686bb09a52b04f9f5d749c05abe8e73
Yeqin Gong <gon@gyeq.in>
2025-01-07 23:12:14 +0800

    修改编辑器设置方式

--- src/lib/user-config.js
+++ src/lib/user-config.js
@@ -18,24 +18,31 @@ export function getUserLanguage() {
 // 编辑器配置
 //
 // 不同编辑器独立设置
-export function putEditorAllConfig(editor, config) {
-  localStorage.setItem(editor, JSON.stringify(config));
-}
-
-export function getEditorAllConfig(editor) {
-  const configStr = localStorage.getItem(editor);
-  return JSON.parse(configStr);
-}
-
 export function putEditorConfig(editor, key, value) {
-  const config = getEditorAllConfig(editor);
-  config[key] = value;
-  putEditorAllConfig(editor);
+  localStorage.setItem(`${editor}:${key}`, value);
 }
 
 export function getEditorConfig(editor, key) {
-  const config = getEditorAllConfig(editor);
-  return config[key];
+  return localStorage.getItem(`${editor}:${key}`);
+}
+
+export function putEditorAllConfig(editor, config = {}) {
+  for (const key in config) {
+    putEditorConfig(editor, key, config[key]);
+  }
+}
+
+export function getEditorAllConfig(editor) {
+  const config = {};
+  const keyId = `${editor}:`;
+  let i, key;
+  for (i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i);
+    if (key.startsWith(keyId)) {
+      config[key.replace(keyId, '')] = localStorage.getItem(`${editor}:${key}`);
+    }
+  }
+  return config;
 }
 
 // 停靠栏是否反转
